aa6b89546207482f0c6e5916420b542f78f061f5
Dinesh Sandaruwan <imdineshsandaru@gmail.com>
2021-08-28 18:05:03 +0530

    Increase the efficiency by sending only the changing values instead of hole text

--- tutorials/beer_wall.py
+++ tutorials/beer_wall.py
@@ -7,13 +7,16 @@ beer_card = page.add('wall', ui.markdown_card(
     box='1 1 4 2',
     title='99 Bottles of Beer',
     content='',
+    data=dict(before='99', after='98')
 ))
 
-for i in range(99, 0, -1):
-    beer_card.content = f"""
-{i} bottles of beer on the wall, {i} bottles of beer.
+beer_card.content = """={{before}} bottles of beer on the wall, {{before}} bottles of beer.
 
-Take one down, pass it around, {i - 1} bottles of beer on the wall...
+Take one down, pass it around, {{after}} bottles of beer on the wall...
 """
+
+for i in range(99, 0, -1):
+    beer_card.data.before=str(i)
+    beer_card.data.after=str(i-1)
     page.save()
     time.sleep(1)
